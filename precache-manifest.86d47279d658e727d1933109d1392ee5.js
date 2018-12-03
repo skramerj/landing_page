7be723296e2a396cc4f72932b63a156cf7899502
Sam Kramer <sam@praxismfg.com>
2018-12-03 16:04:36 -0500

    Updates

---
+++ precache-manifest.86d47279d658e727d1933109d1392ee5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "029464f1a17e3cd8cd9085b6df946d46",
+    "url": "/static/media/quality_control.029464f1.svg"
+  },
+  {
+    "revision": "630c53dbaf9a5109f79871335c763e3a",
+    "url": "/static/media/next_day_delivery.630c53db.svg"
+  },
+  {
+    "revision": "c6b8d20c0b72188a48bf35e918f6bb30",
+    "url": "/static/media/larger_build.c6b8d20c.svg"
+  },
+  {
+    "revision": "3637b41caec4a7a55e5c9df47dc7c472",
+    "url": "/static/media/consistent_tolerances.3637b41c.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e89c5c156494946d597f",
+    "url": "/static/js/main.e89c5c15.chunk.js"
+  },
+  {
+    "revision": "35f9d7fe446d2703d32e",
+    "url": "/static/js/1.35f9d7fe.chunk.js"
+  },
+  {
+    "revision": "8c7d3d296a5a10ba5dd85a83a0d1c9c5",
+    "url": "/index.html"
+  }
+];
