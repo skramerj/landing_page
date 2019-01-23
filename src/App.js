8490d75b15467eed8d8455d08aeb08a01d71a39b
Sam Kramer <sam@praxismfg.com>
2019-01-23 11:43:40 -0500

    fix dynamic screen width

--- src/App.js
+++ src/App.js
@@ -44,10 +44,34 @@ export default class App extends Component {
 			 	</div>
 			</div>)
 
+
   	var typeform = (<div style={{margin: "0px", height: "95vh", padding: "0"}}>
 		  	<iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://samkramer1.typeform.com/to/oIDrqI"></iframe> 
 		  	<script type="text/javascript" src="https://embed.typeform.com/embed.js"></script>
 		 	</div>)
+
+  	if (window.innerWidth < 600) {
+  		to_render = (
+  		<div className="background" style={background_div}>
+				<HeroText />
+				<LeadCaptureButton />
+				<div style={{margin: "0px"}}>
+					<div style={{textAlign: "center"}}>
+						<h2 style={{color: "white", fontWeight: "200", fontFamily: hero_font_1}}>Enter Passcode</h2>
+			  		<input
+			  			style={{color: "Black", width: "150px", height: "30px", fontFamily: hero_font_1, textAlign: "center"}}
+			  			type="text"
+			  			autoFocus
+			  			value={this.state.passcode.toUpperCase()}
+			  			tabIndex="0"
+			  			onChange={this.handleChange}
+			  			>
+			  		</input>
+			  	</div>
+			 	</div>
+			</div>)
+
+  	}
 
 		if (this.state.passcode === "ALPHA") {
 			to_render = typeform
